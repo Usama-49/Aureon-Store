7b563a5f3d878dd3d85846a227d0018aba3ecce9
Usama <usamahere49@gmail.com>
2026-09-15 13:02:27 +0500

    Nodemailer configration for production

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -2,11 +2,17 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 5000,
+  socketTimeout: 10000,
 });
 
 const sendRegistrationEmail = async (userMail, verifyUrl) => {
